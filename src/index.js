fc2018a258f196a4cfb9313f39f3a26f736ac8d0
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-07 22:21:33 +0300

    Basic webpack configuration: prod and development mode, entry and ouput, htmlWebpackPlugin, dynamic file name

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {calc} from "./test";
+
+console.log(123 )
+console.log(calc(5,6))
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        //path is needed to support different operating systems
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+        },
+        plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})],
+    }
+
+}
