3ab0d4fd4ab5e66e64c8e716ba15d7c2afbc4bd3
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-07 23:02:16 +0300

    Changing webpack.config.js to typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,9 @@
-const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require('webpack');
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = (env) => {
-    return {
+export default (env: any) => {
+    const config: webpack.Configuration = {
         mode: env.mode ?? 'development',
         //path is needed to support different operating systems
         entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -30,5 +30,5 @@ module.exports = (env) => {
         },
 
     }
-
+return config
 }
