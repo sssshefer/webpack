88b6b4ccc3a2032f90aad7701a0260795a10cccb
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-07 23:44:06 +0300

    Adding react

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const App = () => {
+    const [count, setCount] = React.useState(0)
+
+    const increment = () => setCount(prevState => prevState + 1)
+    return (
+        <div>
+            <h1>
+                {count}
+            </h1>
+            <button onClick={increment}>inc</button>
+        </div>
+    );
+};
+
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import {calc} from "./test";
-
-console.log(123 )
-console.log(calc(53,6))
