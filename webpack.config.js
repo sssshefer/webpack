30b2418e5d183890c11b07cffe8bdbd69ebab385
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-07 22:37:03 +0300

    Progress plugin. Shows percentage of loaded build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = (env) => {
     return {
@@ -11,7 +12,11 @@ module.exports = (env) => {
             filename: '[name].[contenthash].js',
             clean: true,
         },
-        plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})],
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+            new webpack.ProgressPlugin()
+        ],
+
     }
 
 }
