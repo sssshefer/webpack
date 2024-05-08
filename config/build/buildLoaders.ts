aa45797d392c70d8ba6b5dc70105f785e70da0c7
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-08 14:40:38 +0300

    Assets support. Svgr plugin to work with svg like with react components

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,11 +5,36 @@ import {BuildOptions} from "./types/types";
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
 
+    const assetLoader = {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    }
+
+    const svgLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [{
+            loader: '@svgr/webpack',
+            options: {
+                icon: true,
+                svgoConfig: {
+                    plugins: [
+                        {
+                            name: 'convertColors',
+                            params: {
+                                currentColor: true
+                            }
+                        }
+                    ],
+                }
+            }
+        }],
+    }
     const cssLoaderWithModules = {
         loader: "css-loader",
         options: {
             modules: {
-                localIdentName:isDev? '[path][name]__[local]':'[hash:base64:8]',
+                localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
             }
         },
     }
@@ -31,7 +56,10 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     }
     return [
         //order of rules is important
+        assetLoader,
         scssLoader,
-        tsLoader
+        tsLoader,
+        svgLoader,
+
     ]
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import cl from './App.module.scss';
 import {Link, Outlet} from "react-router-dom";
 import {About} from "@/pages/About";
+import avatarPng from '@/assets/avatar.png'
+import avatarJpg from '@/assets/avatar.jpg'
+import AppImage from '@/assets/app-image.svg'
 
 export const App = () => {
     const [count, setCount] = React.useState(0)
@@ -18,6 +21,9 @@ export const App = () => {
             <button className={cl.button} onClick={increment}>inc</button>
             <Outlet/>
             <About/>
+            <img width={100} src={avatarPng} alt="pic"/>
+            <img width={100} src={avatarJpg} alt="pic"/>
+            <AppImage style={{color:'blue'}} width={50} height={50}/>
         </div>
     );
 };
