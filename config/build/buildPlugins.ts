7fd7d60c4aa97499e395507b753e186f5357f38f
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-08 16:34:15 +0300

    CopyPlugin for other file which must be in final build

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,13 +7,14 @@ import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 
 export function buildPlugins({mode, paths, analyzer, platform}: BuildOptions): Configuration['plugins'] {
     const isDev = mode === 'development';
     const isProd = mode === 'production';
 
     const plugins: Configuration['plugins'] = [
-        new HtmlWebpackPlugin({template:paths.html, favicon:path.resolve(paths.public, 'favicon.ico')}),
+        new HtmlWebpackPlugin({template: paths.html, favicon: path.resolve(paths.public, 'favicon.ico')}),
         new DefinePlugin({
             __PLATFORM__: JSON.stringify(platform)
         }),
@@ -32,9 +33,13 @@ export function buildPlugins({mode, paths, analyzer, platform}: BuildOptions): C
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }))
+        plugins.push(new CopyPlugin({
+                patterns: [
+                    {from:path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales')},],
+            }),)
     }
 
-    if(analyzer){
+    if (analyzer) {
         plugins.push(new BundleAnalyzerPlugin())
 
     }
